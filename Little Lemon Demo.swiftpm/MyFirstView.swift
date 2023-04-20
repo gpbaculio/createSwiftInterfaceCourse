4471167f795a562b0ae350b1e085f8438fadacd2
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-20 18:32:26 +0800

    init

---
+++ Little Lemon Demo.swiftpm/MyFirstView.swift
@@ -0,0 +1,21 @@
+//
+//  MyFirstView.swift
+//  Little Lemon Demo
+//
+//  Created by Glendon Philipp Baculio on 4/20/23.
+//
+
+import SwiftUI
+
+struct MyFirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Little Lemon")
+    }
+}
+
+struct MyFirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFirstView()
+    }
+}
